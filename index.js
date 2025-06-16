5cedd621f27c9c4e61b44c3d54a18dbe1dc578af
Ryan <ryannjueleo0987@gmail.com>
2025-06-16 21:38:49 +0300

    Initial commit or recent changes

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const sgMail = require("@sendgrid/mail");
+const { Resend } = require("resend");
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 app.use(express.json());
 
 app.post("/send-email", async (req, res) => {
   const { to, subject, text } = req.body;
 
-  const msg = {
-    to,
-    from: process.env.SENDER_EMAIL, // must be your verified sender
-    subject,
-    text,
-  };
-
   try {
-    await sgMail.send(msg);
+    const data = await resend.emails.send({
+      from: process.env.VERIFIED_SENDER,
+      to,
+      subject,
+      text,
+    });
+
+    console.log("Email sent:", data);
     res.status(200).send("Email sent successfully");
   } catch (error) {
     console.error("Error sending email:", error);
